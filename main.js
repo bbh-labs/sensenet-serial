81610d083917fcf2d9466a9ce1033b5af9fec05a
Jacky Boen <jacky.boen@bartleboglehegarty.com>
2016-02-03 13:29:09 +0800

    Use binary data packets instead of JSON to save transfer bandwidth
    Signed-off-by: Jacky Boen <jacky.boen@bartleboglehegarty.com>

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ const serialport = require('serialport');
 const SerialPort = serialport.SerialPort;
 let serialPort = new SerialPort('/dev/ttyACM0', {
 	baudrate: 9600,
-	parser: serialport.parsers.readline('\r\n'),
 });
 
 // Parse
@@ -34,18 +33,56 @@ process.on('SIGINT', function() {
 	process.exit(0);
 });
 
+let buffer = new Buffer(0);
+
 serialPort.on('open', function() {
-	serialPort.on('data', function(rawData) {
-		try {
-			let data = JSON.parse(rawData);
-			pusher.trigger('private-client-reading', 'client-reading', {
-				deviceID: data.deviceID,
-				data: data,
-				//coordinate: coordinate,
-			});
-		} catch (error) {
-			console.log(error);
-			// do nothing
+	serialPort.on('data', function(data) {
+		// Append data to global buffer
+		buffer = Buffer.concat([buffer, data]);
+
+		let start = 0;
+		let end = buffer.indexOf('\r\n', start);
+		let reading;
+
+		while (end - start >= 28) {
+			let workBuffer = buffer.slice(end - 28, end);
+			let deviceID = workBuffer.toString('utf-8', 0, 10);
+			let temperature = workBuffer.readFloatLE(10);
+			let humidity = workBuffer.readFloatLE(14);
+			let uv = workBuffer.readFloatLE(18);
+			let particles = workBuffer.readFloatLE(22);
+			let carbonMonoxide = workBuffer.readUInt16LE(26);
+			reading = {
+				deviceID: deviceID,
+				temperature: temperature,
+				humidity: humidity,
+				uv: uv,
+				particles: particles,
+				carbonMonoxide: carbonMonoxide,
+			};
+
+			start = end + 2;
+			end = buffer.indexOf('\r\n', start);
+		}
+
+		if (reading) {
+			console.log(reading);
+			sendData(reading);
+		}
+
+		// Remove processed data from global buffer
+		if (end >= 0) {
+			let newbuffer = new Buffer(end - start);
+			buffer.copy(newbuffer, 0, start, end);
+			buffer = newbuffer;
 		}
 	});
 });
+
+function sendData(data) {
+	pusher.trigger('private-client-reading', 'client-reading', {
+		deviceID: data.deviceID,
+		data: data,
+		//coordinate: coordinate,
+	});
+}
